3edba2a329928860d36daeb31d30afeb72ce04c3
ShellyIsaacs <45702961+ShellyIsaacs@users.noreply.github.com>
2023-03-18 04:03:59 +0000

    First draft

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  config.web_console.whitelisted_ips = ['107.220.204.111', '107.16.0.0/12', '192.168.0.0/16']
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
